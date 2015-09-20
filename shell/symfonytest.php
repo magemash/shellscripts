57b387ad5ec734174405e4b8e0d8001d416dda71
Scott Pringle <scottp99@gmail.com>
2015-09-20 09:55:44 +0100

    updated symfony test

--- shell/symfonytest.php
+++ shell/symfonytest.php
@@ -3,13 +3,21 @@
 require_once '../app' . DIRECTORY_SEPARATOR . 'Mage.php';
 
 use Symfony\Component\Console\Application;
-use MagentoCommand\Command\HelloworldCommand;
+use Symfony\Component\Finder\Finder;
+use Symfony\Component\Yaml\Parser;
 
 $console = new Application();
-$console->add(new HelloworldCommand());
 
-// add all commands
+$finder = new Finder();
+$finder->files()->in('../vendor')->name('*config.yml');
+$yaml = new Parser();
 
+foreach ($finder as $file) {
+    $content = $yaml->parse(file_get_contents($file));
 
+    foreach ($content['commands'] as $command) {
+        $console->add(new $command());
+    }
+}
 
 $console->run();
